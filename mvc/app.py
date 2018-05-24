6a50911954ef206636fb66c4396ae589e70afdae
Yash Patel <yash.p@digitalshelfiq.com>
2018-05-24 11:55:42 +0530

    changed name router to application

---
+++ mvc/app.py
@@ -0,0 +1,34 @@
+import os
+from werkzeug.wrappers import Request, Response
+from werkzeug.routing import Map, Rule
+from werkzeug.exceptions import HTTPException, NotFound
+from werkzeug.wsgi import SharedDataMiddleware
+from werkzeug.utils import redirect
+from jinja2 import Environment, FileSystemLoader
+
+
+class Application(object):
+
+    def __init__(self, config={}):
+        pass
+
+    def dispatch_request(self, request):
+        return Response('Hello World!')
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+
+def create_app(redis_host='localhost', redis_port=6379, with_static=True):
+    app = Application()
+
+    if with_static:
+        app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+            '/static':  os.path.join(os.path.dirname(__file__), 'static')
+        })
+    return app
--- mvc/router.py
+++
@@ -1,8 +0,0 @@
-import os
-from werkzeug.wrappers import Request, Response
-from werkzeug.routing import Map, Rule
-from werkzeug.exceptions import HTTPException, NotFound
-from werkzeug.wsgi import SharedDataMiddleware
-from werkzeug.utils import redirect
-from jinja2 import Environment, FileSystemLoader
-
